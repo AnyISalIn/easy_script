7d48f0336d408d0cfe1c8076d2f0bdabcec9f541
anyisalin <anyisalin@gmail.com>
2016-12-01 08:21:21 -0500

    fix bug

--- vaneq_cluster_deploy.sh
+++ vaneq_cluster_deploy.sh
@@ -2,7 +2,7 @@
 #
 # vaneq cluster build script
 
-nodes=('192.168.10.101' '192.168.10.202')
+nodes=('172.24.1.136')
 read -p 'please input master node ipaddress : ' master_node
 APP_ROOT='/var/www/miq/vmdb'
 
@@ -17,7 +17,7 @@ function run_test {
     log INFO $1
   else
     log ERROR $2
-    exec $3
+    $3
   fi
 }
 
@@ -25,19 +25,19 @@ function connect_test {
   [ -x ${nodes[*]} ] || ( echo 'please input node'; exit 1 )
   for node in ${nodes[*]}; do
     ping  -W 3  -c 4  ${node} > /dev/null
-    run_test "${node} is active" "${node} is inactive" "break"
+    run_test "${node} is active" "${node} is inactive" "exit 1"
   done
 }
 
 function ssh_trust {
   ssh-keygen -t rsa -P '' -f ~/.ssh/vaneq_rsa
   log INFO "Please input localhost password"
-  ssh-copy-id ~/.ssh/id_rsa localhost 2> /dev/null
+  ssh-copy-id  -i ~/.ssh/vaneq_rsa localhost 2> /dev/null
   if [[ $? -eq 0 ]]; then
      for node in ${nodes[*]}; do
        log INFO "Please input ${node} password"
-       scp ~/.ssh/{authorized_keys,vaneq_rsa} $node:~/.ssh/
-       run_test "${node} ssh trust!" "${node} ssh untrust!" "break"
+       scp ~/.ssh/{authorized_keys,vaneq_rsa} ${node}:~/.ssh/
+       run_test "${node} ssh trust!" "${node} ssh untrust!" "exit 1"
       done
   else
     log ERROR "wrong password"
@@ -46,8 +46,8 @@ function ssh_trust {
 
 function sync_key {
   for node in ${nodes}; do
-    scp ${APP_ROOT}/certs/* node:${APP_ROOT}/certs/
-    run_test "${node} sync success" "${node} sync failed, please check network or ssh trust" "break"
+    scp -i ~/.ssh/vaneq_rsa ${APP_ROOT}/certs/* ${node}:${APP_ROOT}/certs/
+    run_test "${node} sync success" "${node} sync failed, please check network or ssh trust" "exit 1"
   done
 }
 
@@ -62,19 +62,19 @@ function encrypt {
 function sync_database_config {
   cd ${APP_ROOT}
   for node in ${nodes}; do
-    scp /tmp/tmp_db.settings ${node}:${APP_ROOT}/config/
-    run_test "${node} Database Config File Copy Success!" "${node} Database Config File Copy Failed!" "break"
+    scp -i ~/.ssh/vaneq_rsa /tmp/tmp_db.settings ${node}:${APP_ROOT}/config/database.yml
+    run_test "${node} Database Config File Copy Success!" "${node} Database Config File Copy Failed!" "exit 1"
   done
   rm -rf /tmp/tmp_db.settings
 }
 
 function start_evmserver {
   for node in ${nodes}; do
-    ssh ${node} -- systemctl restart evmserverd
+    ssh -i ~/.ssh/vaneq_rsa ${node} -- systemctl restart evmserverd
     run_test "${node} EVM Server Started Success!" "${node} EVM Server Started Failed!" "continue"
   done
 }
 
 for fun in connect_test ssh_trust sync_key encrypt sync_database_config start_evmserver; do
-    exec $fun || echo 'failed'
+    ${fun} || echo '${fun} failed'
 done
